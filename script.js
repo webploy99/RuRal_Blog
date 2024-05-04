c765a41e53a11a9a68d59aa7afa8f7fd1e1312ca
webploy99 <webploy99@gmail.com>
2024-05-04 11:22:01 +0700

    update commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
     // Add active class to the current button (highlight it)
     var header = document.getElementById("myActive");
     var btns = header.getElementsByClassName("btn");
@@ -30,20 +31,36 @@
 
 
 // change page when click
-    const $ = document.querySelector.bind(document),
-        $$ = document.querySelectorAll.bind(document);
+    const first = document.querySelector.bind(document),
+        second = document.querySelectorAll.bind(document);
 
-    const ruralContent = $$(".rural-item"),
-        boxContent = $$(".box-item");
+    const ruralContent = second(".rural-item"),
+        boxContent = second(".box-item ");
 
     ruralContent.forEach((rural, index) => {
         const newBox = boxContent[index];
         rural.onclick = function() {
-            $(".rural-item.active").classList.remove("active")
-            $(".box-item.active").classList.remove("active")
+            first(".rural-item.active").classList.remove("active")
+            first(".box-item.active").classList.remove("active")
 
             rural.classList.add ("active")
             newBox.classList.add("active");
             console.log(rural)
         }
     })
+
+    // owlCarousel
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        items: 1,
+        margin: 10,
+        // nav: true,
+        // dots: true,
+    //   responsive: {
+    //     20: {
+    //       items: 1.1,
+    //       nav: false,
+    //     },
+    //   },
+    });
